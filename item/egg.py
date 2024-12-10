38a4988f457e6146a5197f48b7308968ff5cfaca
EmsiaetKadosh <s3320076100@163.com>
2024-12-10 17:35:17 +0800

    setup

--- item/egg.py
+++ item/egg.py
@@ -6,12 +6,3 @@ from text import Description
 class Egg(Item):
 	def __init__(self, name: str, description: Description, texture: Resource):
 		super().__init__(name, description, texture)
-		
-
-class A:
-	def __init__(self, a: int):
-		self.i = a
-
-
-class B(A):
-	def __init__(self, b: int):
